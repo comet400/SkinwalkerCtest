b253f2585a822148229694aa66816dc216df0dd2
comet400 <135397104+comet400@users.noreply.github.com>
2024-06-09 03:56:06 -0400

    Update HeaderFile.h

--- HeaderFile.h
+++ HeaderFile.h
@@ -1,1 +1,21 @@
-#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#pragma warning(disable:4996)
+
+class Skinwalker
+{
+
+};
+
+class farmers
+{
+
+};
+
+class player
+{
+	
+};
