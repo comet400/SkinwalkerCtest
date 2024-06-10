6739b8bd6c4fe5ddbe9ff0853b1c256f44dc9bf7
RobinNor114 <150862204+RobinNor114@users.noreply.github.com>
2024-06-09 22:53:25 -0400

    Update HeaderFile.h

--- HeaderFile.h
+++ HeaderFile.h
@@ -7,15 +7,42 @@
 
 class Skinwalker
 {
+	int infect;
 
 };
 
 class farmers
 {
+private:
 
+	int farmerPlant;
+	int farmerWeld;
+	int famerFish;
+	int farmerHunt;
 };
 
 class player
 {
-	
+private:
+
+	int playerHunger;
+	int playerThirst;
+	int playerSanity;
+	int playerPain;
+	int playerEnergy;
+	int uniquePlayerItems;
+	int consumablePlayerItems;
 };
+
+/* player commands.
+* 1. search: Look around the village.
+* 2. Hunt: Go hunting for food.
+* 3. Heal: Heal yourself. (medication required)
+* 4. Chat: Talk to villagers.
+* 5. Plant: Plant seeds. (seeds required)
+* 6. Fish: Go fishing.
+* 7. Sleep: Go to next day.
+* 8. Shoot: shoot suspected skin-walker. (fire-arm required) 
+* 9. Help: Help with commands.
+* 10. Gift: give villager an item.
+*/
